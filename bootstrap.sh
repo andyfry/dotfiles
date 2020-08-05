afdcbe88331208be4c7841e1d3761fce7ba15210
Andy Fry <andrewdalefry@gmail.com>
2020-08-05 16:55:36 -0500

    "updated bootstrap.sh"

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # Make sure we are running in the directory this file is in
 DIR=$(dirname "$0")
 cd "$DIR"
-# . scripts/functions.sh
+. scripts/functions.sh
 
 echo "running Bootstrap"
 info "Running Bootstrap"
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,5 +1,23 @@
 #! /usr/bin/env sh
 
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
 symlink() {
     OVERWRITTEN=""
     if [ -e "$2" ] || [ -h "$2" ]; then
@@ -23,57 +41,4 @@ clear_broken_symlinks() {
             substep_error "Failed to remove broken symlink at $fn."
         fi
     done
-}
-
-# Took these printing functions from https://github.com/Sajjadhosn/dotfiles
-coloredEcho() {
-    local exp="$1";
-    local color="$2";
-    local arrow="$3";
-    if ! [[ $color =~ '^[0-9]$' ]] ; then
-       case $(echo $color | tr '[:upper:]' '[:lower:]') in
-        black) color=0 ;;
-        red) color=1 ;;
-        green) color=2 ;;
-        yellow) color=3 ;;
-        blue) color=4 ;;
-        magenta) color=5 ;;
-        cyan) color=6 ;;
-        white|*) color=7 ;; # white or invalid color
-       esac
-    fi
-    tput bold;
-    tput setaf "$color";
-    echo "$arrow $exp";
-    tput sgr0;
-}
-
-
-
-message() {
-    coloredEcho "$1" white "="
-}
-
-info() {
-    coloredEcho "$1" blue "========>"
-}
-
-success() {
-    coloredEcho "$1" green "========>"
-}
-
-error() {
-    coloredEcho "$1" red "========>"
-}
-
-substep_info() {
-    coloredEcho "$1" magenta "===="
-}
-
-substep_success() {
-    coloredEcho "$1" cyan "===="
-}
-
-substep_error() {
-    coloredEcho "$1" red "===="
 }
