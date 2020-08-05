ec5101381a9b9371a9f2b4f7d777f1d2ad866b18
Andy Fry <andrewdalefry@gmail.com>
2020-08-05 08:54:41 -0500

    "changed to use homebrew to install xcode"

--- newmac.sh
+++ newmac.sh
@@ -14,6 +14,8 @@ success () {
 
 fail () {
   printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
 }
 
 echo ''
@@ -26,22 +28,12 @@ if sudo -v; then
     while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
     success "Sudo credentials updated."
 else
-    error "Failed to obtain sudo credentials."
-fi
-
-info "Installing XCode command line tools..."
-if xcode-select --print-path &>/dev/null; then
-    success "XCode command line tools already installed."
-elif xcode-select --install &>/dev/null; then
-    success "Finished installing XCode command line tools."
-else
-    fail "Failed to install XCode command line tools."
+    fail "Failed to obtain sudo credentials."
 fi
 
 info "Installing Homebrew package manager"
-if test ! $(which brew)
-then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if test ! $(which brew); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   success "Finished installing Homebrew."
 else
   success "Homebrew already installed."
