9314fa19d285b36e505bfe83d0cb8359904c3fa9
Andy Fry <andrewdalefry@gmail.com>
2020-08-16 07:40:25 -0500

    Added a function to setup my terminal windows

---
+++ config/fish/functions/start.fish
@@ -0,0 +1,10 @@
+function start
+	kitty @ launch --type=os-window --title gotop
+	kitty @ launch --type=os-window --title neofetch
+	kitty @ launch --type=os-window --title todo
+	kitty @ launch --type=os-window --title clock
+	kitty @ send-text --match title:gotop gotop\n 
+	kitty @ send-text --match title:clock clock\n
+	kitty @ send-text --match title:neofetch neofetch\n
+	kitty @ send-text --match title:todo mdv todo.md\n 
+end
